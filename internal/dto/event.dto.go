0b0cc15529b9002ec727dc0650bbdd834d661616
Rio William <riowilliamsimamora@gmail.com>
2024-08-06 15:03:18 +0700

    first init new event

---
+++ internal/dto/event.dto.go
@@ -0,0 +1,34 @@
+package dto
+
+import (
+	"errors"
+	"monorepo/internal/constants"
+	"time"
+)
+
+type RequestCreateEvent struct {
+	LocationID string    `json:"location_id" validate:"required"`
+	Status     string    `json:"status,omitempty"`
+	Type       string    `json:"type" validate:"required,oneof=holiday appointment"`
+	StartTime  time.Time `json:"start_time" validate:"required"`
+	EndTime    time.Time `json:"end_time"`
+}
+
+type ResponseCreateEvent struct {
+	ID         string    `json:"id,omitempty"`
+	ProfileID  *string   `json:"profile_id,omitempty"`
+	LocationID string    `json:"location_id,omitempty"`
+	Status     string    `json:"status,omitempty"`
+	Type       string    `json:"type,omitempty"`
+	StartTime  time.Time `json:"start_time,omitempty"`
+	EndTime    time.Time `json:"end_time,omitempty"`
+}
+
+func (r RequestCreateEvent) Validate() error {
+	if r.Type != constants.Appointment && r.Status != "" {
+		err := errors.New("status should only be provided when type is appointment")
+		return err
+	}
+
+	return nil
+}
